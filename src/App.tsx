de39a0978633de1e6660cdb600e4a131cf5fe6b4
Alyona Nabok <alyonana42@gmail.com>
2024-04-05 21:33:03 +0300

    add error notification

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 
+import { message } from 'antd';
 import { fetchIssues } from './redux/slices/issuesSlice';
-import Header from './components/header'
-
-import Kanban from './components/kanban';
 import { useAppDispatch, useAppSelector } from './redux/hooks';
 import { fetchRepo } from './redux/slices/repoSlice';
+import Header from './components/header'
+import Kanban from './components/kanban';
 
 function App() {
   const { issues, loading: issuesLoading, error: issuesError } = useAppSelector((state) => state.issues);
   const { repo, loading: repoLoading, error: repoError } = useAppSelector((state) => state.repo);
   const dispatch = useAppDispatch();
+  const [messageApi, contextHolder] = message.useMessage();
 
-  console.log(repo)
+  if (issuesError || repoError) {
+    messageApi.open({
+      type: 'error',
+      content: issuesError || repoError,
+    });
+  }
 
   const handleLoadIssues = (url: string) => {
     dispatch(fetchIssues(url))
@@ -21,6 +27,7 @@ function App() {
 
   return (
     <>
+      {contextHolder}
       <Header onLoadIssues={handleLoadIssues} loading={issuesLoading || repoLoading} />
       <Kanban issues={issues} repoInfo={repo} />
     </>
